4311c2da54cb533dfe9928cd00f0faf6e8a437fa
Mariia <MK193@yandex.ru>
2022-09-01 00:06:46 +0300

    Task repository created

---
+++ ex_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второe число");
+int b = Convert.ToInt32(Console.ReadLine());
+    if (a>b) 
+    {
+        Console.WriteLine("Макисмальное число "+a);
+    } 
+    else if (b>a)
+    {
+        Console.WriteLine("Максимальное число "+b);
+    }
+        else Console.WriteLine("Числа равны");
---
+++ ex_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите 1 число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int z = Convert.ToInt32(console.ReadLine());
+
+int max = x;
+
+if (max>y)
+{
+    if (max>z)
+    Console.WriteLine("Максимальное число" +max);
+    else max = y;
+}
