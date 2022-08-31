eb5e2722cbd71c50b25c71ef9d55867118a49a26
Mariia <MK193@yandex.ru>
2022-09-01 00:14:26 +0300

    Ex_4 done,test and work

--- ex_4/Program.cs
+++ ex_4/Program.cs
@@ -5,13 +5,18 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число");
 int y = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 3 число");
-int z = Convert.ToInt32(console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
 
 int max = x;
 
 if (max>y)
 {
     if (max>z)
-    Console.WriteLine("Максимальное число" +max);
-    else max = y;
+    Console.WriteLine("Максимальное число " +max);
 }
+else max = y;
+if (max>z)
+{
+    Console.WriteLine("Максимальное число " +max);
+}
+else Console.WriteLine("Максимальное число " +z);
