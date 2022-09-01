e2893f850e4497bfe4886caa57728e3a6bcca761
Mariia <MK193@yandex.ru>
2022-09-01 11:34:16 +0300

    Ex_4 done and check

--- ex_4/Program.cs
+++ ex_4/Program.cs
@@ -1,22 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Введите 1 число");
+Console.WriteLine("Введите 1 целое число");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2 число");
+Console.WriteLine("Введите 2 целое число");
 int y = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 3 число");
+Console.WriteLine("Введите 3 целое число");
 int z = Convert.ToInt32(Console.ReadLine());
 
 int max = x;
-
-if (y>max)
-{
-    max=y;
-}
-else max=x;
-if (z>max)
-{
-max=z;
-}
-else max=y;
-Console.WriteLine("Максимальное число " +max);
+if (y > max)
+    {
+        max = y;
+    }
+            if (z>max)
+            {
+                max = z;
+                Console.WriteLine("Максимальное число " +max);
+            }
+            else Console.WriteLine("Максимальное число " +max);
