037836f511d309569843c27e2bcde6d82aa5f662
Mariia <MK193@yandex.ru>
2022-09-01 11:20:33 +0300

    Ex_8 done and check

--- ex_8/Program.cs
+++ ex_8/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i<=n)
+    {
+        Console.Write(i+",");
+        i = i + 2;
+    }
+
